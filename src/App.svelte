5d5d9df1564f1e7bcafde5523afce29bf0f824ba
gitmhjao <ntumhjao@gmail.com>
2023-09-29 07:24:30 +0800

    'Day13'

---
+++ src/App.svelte
@@ -0,0 +1,48 @@
+<!-- 在 Javascript 當中 import Counter -->
+<script>
+  import Counter from './lib/Counter.svelte';
+
+  let count = 100;
+
+  let someState = 'TheGreatSvelte';
+  const sparkle = (text) => {
+    const sparkles = ['★', '☆', '✧', '✪'];
+    const randomSparkles = () => sparkles[Math.floor(Math.random() * sparkles.length)];
+    const sparkledText = text.split('').reduce((a, c) => a + randomSparkles() + c, '');
+    return sparkledText;
+  }
+
+  const href = 'https://ithelp.ithome.com.tw/users/20120178/ironman/7031';
+
+  const handleClick = (e) => {
+    console.log(e);
+    // count += e.detail;
+  }
+</script>
+
+<main>
+  <!-- 在 HTML 當中直接嵌入 Counter -->
+  <Counter {count} on:changeCount={handleClick}/>
+
+  <p class='comment'>Check out <a {href}>Svelte Tutorial</a>, the awesome article powered by {sparkle(someState)}!</p>
+</main>
+
+<style>
+  main {
+    min-height: 100vh;
+    max-width: 500px;
+    margin: 0 auto;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+  }
+
+  p.comment {
+    color: #888;
+    margin: 1em 0.5em;
+  }
+
+  p.comment a {
+    text-decoration: none;
+  }
+</style>
