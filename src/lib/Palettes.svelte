4d6d95c1a827be6acd4debd01b891a4f602c5b91
githubmhjao <ntumhjao@gmail.com>
2023-10-03 23:43:28 +0800

    'Day18'

---
+++ src/lib/Palettes.svelte
@@ -0,0 +1,67 @@
+<script>
+  import unlock from "../assets/unlock.svg";
+  export let palettes;
+</script>
+
+<div class="palettes">
+  {#each palettes as { hex, id } (id)}
+    <div class="card">
+      <div class="palette" style="background: #{hex}" />
+      <div class="hex-code">
+        <p>
+          {hex}
+        </p>
+      </div>
+      <div class="lock-icon">
+        <img src={unlock} alt="color-unlocked" />
+      </div>
+    </div>
+  {:else}
+    <div class="card no-color">
+      <div>
+        <p>No color to show. Please add a color.</p>
+      </div>
+    </div>
+  {/each}
+</div>
+
+<style>
+  .palettes {
+    margin: 1em;
+    flex: 1;
+    display: grid;
+    gap: 0.5em;
+  }
+
+  .card {
+    padding: 0.5em;
+    background: var(--color-bg-3);
+    border-radius: var(--border-radius);
+    display: grid;
+    grid-template-columns: 1fr 2fr 0.5fr;
+    align-items: center;
+  }
+
+  .card.no-color {
+    font-size: 1.5em;
+    grid-template-columns: 1fr;
+  }
+
+  .card .palette {
+    height: 100%;
+    border-radius: 0.2em;
+  }
+
+  .card .lock-icon {
+    display: flex;
+    width: 3em;
+    height: 3em;
+    padding: 0.7em;
+    opacity: 0.3;
+    cursor: pointer;
+  }
+
+  .card .lock-icon:hover {
+    opacity: 0.5;
+  }
+</style>
